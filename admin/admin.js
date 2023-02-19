0e7b4dccd8be9a1ee47f5cb5c77f90ca828a7453
SurajGupta1001 <suraj.kumargupta.it24@heritageit.edu.in>
2023-02-19 12:40:56 +0530

    push

---
+++ admin/admin.js
@@ -0,0 +1,24 @@
+const AdminJS = require('adminjs')
+const AdminJSExpress = require('@adminjs/express')
+const AdminJSMongoose = require('@adminjs/mongoose')
+
+const Movie = require("../models/movie.models")
+const Person = require("../models/person.model")
+const User = require("../models/user.models")
+
+AdminJS.registerAdapter({
+  Resource: AdminJSMongoose.Resource,
+  Database: AdminJSMongoose.Database
+})
+
+const adminOptions = {
+  resources: [Movie,Person,User]
+}
+const admin = new AdminJS(adminOptions)
+
+const adminRouter = AdminJSExpress.buildRouter(admin)
+
+const connectAdmin = app => {
+  app.use(admin.options.rootPath, adminRouter)
+}
+module.exports = connectAdmin
