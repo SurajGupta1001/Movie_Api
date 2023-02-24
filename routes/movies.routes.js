91a22e51e6684f9e6a96d7f62dc1b5d6e31ce9c5
vikraj01 <99103720+vikraj01@users.noreply.github.com>
2023-02-24 19:29:22 +0530

    Update

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -87,6 +87,12 @@ router.get("/movie/top_rated", async (req, res, next) => {
   res.json();
 });
 
+
+router.get("/movie/images/testing/", (req,res,next) => {
+    console.log(req.query);
+    res.send(req.query)
+})
+// http://localhost:4000/movie/images/testing/?year=100&&image=1.2
 router.get("/movie/upcoming", async (req, res, next) => {
   res.json();
 });
